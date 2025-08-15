01f975874fa69809e366d235f337057737e308b3
She1ri <alexshelepalo8@gmail.com>
2025-08-15 20:34:52 +0300

    Token

--- WebMacDonalds/WebMacDonalds/Controllers/AccountController.cs
+++ WebMacDonalds/WebMacDonalds/Controllers/AccountController.cs
@@ -2,38 +2,52 @@
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using WebMacDonalds.Data.Entities.Identity;
+using WebMacDonalds.Interfaces;
 using WebMacDonalds.Models.Account;
+using WebMacDonalds.Services;
 
-namespace WebMacDonalds.Controllers
+namespace WebMacDonalds.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class AccountController(IJwtTokenService jwtTokenService, UserManager<UserEntity> userManager) : ControllerBase
 {
-    [Route("api/[controller]")]
-    [ApiController]
-    public class AccountController(UserManager<UserEntity> userManager) : ControllerBase
+    [HttpPost("login")]
+    public async Task<IActionResult> Login([FromBody] LoginModel model)
     {
-        [HttpPost("register")]
-        public async Task<IActionResult> Register([FromForm] RegisterModel model)
+        var user = await userManager.FindByEmailAsync(model.Email);
+        if (user != null && await userManager.CheckPasswordAsync(user, model.Password))
         {
-            if (!ModelState.IsValid)
-            {
-                return BadRequest(ModelState);
-            }
-            var user = new UserEntity
-            {
-                UserName = model.Email,
-                Email = model.Email,
-                FirstName = model.FirstName,
-                LastName = model.LastName
-            };
-            var result = await userManager.CreateAsync(user, model.Password);
-            if (result.Succeeded)
-            {
-                return Ok(new { Message = "User registered successfully." });
-            }
-            foreach (var error in result.Errors)
-            {
-                ModelState.AddModelError(string.Empty, error.Description);
-            }
+            var token = await jwtTokenService.CreateTokenAsync(user);
+            return Ok(new { Token = token });
+        }
+        return Unauthorized("Invalid email or password");
+    }
+
+    [HttpPost("register")]
+    public async Task<IActionResult> Register([FromForm] RegisterModel model)
+    {
+        if (!ModelState.IsValid)
+        {
             return BadRequest(ModelState);
         }
+        var user = new UserEntity
+        {
+            UserName = model.Email,
+            Email = model.Email,
+            FirstName = model.FirstName,
+            LastName = model.LastName
+        };
+        var result = await userManager.CreateAsync(user, model.Password);
+        if (result.Succeeded)
+        {
+            return Ok(new { Message = "User registered successfully." });
+        }
+        foreach (var error in result.Errors)
+        {
+            ModelState.AddModelError(string.Empty, error.Description);
+        }
+        return BadRequest(ModelState);
     }
+
 }
