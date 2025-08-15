8ce97b42858a11fa689f3146353745742da4897a
Artem0904 <artemshadiuk@gmail.com>
2025-08-15 19:54:08 +0300

    register

---
+++ WebMacDonalds/WebMacDonalds/Controllers/AccountController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using WebMacDonalds.Data.Entities.Identity;
+using WebMacDonalds.Models.Account;
+
+namespace WebMacDonalds.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AccountController(UserManager<UserEntity> userManager) : ControllerBase
+    {
+        [HttpPost("register")]
+        public async Task<IActionResult> Register([FromForm] RegisterModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+            var user = new UserEntity
+            {
+                UserName = model.Email,
+                Email = model.Email,
+                FirstName = model.FirstName,
+                LastName = model.LastName
+            };
+            var result = await userManager.CreateAsync(user, model.Password);
+            if (result.Succeeded)
+            {
+                return Ok(new { Message = "User registered successfully." });
+            }
+            foreach (var error in result.Errors)
+            {
+                ModelState.AddModelError(string.Empty, error.Description);
+            }
+            return BadRequest(ModelState);
+        }
+    }
+}
