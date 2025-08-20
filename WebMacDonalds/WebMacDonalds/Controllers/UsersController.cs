496ed760eb9ad11c206aaa109bc6b61cd39d3b43
novakvova <novakvova@gmail.com>
2025-08-20 18:33:29 +0300

    Get list users

---
+++ WebMacDonalds/WebMacDonalds/Controllers/UsersController.cs
@@ -0,0 +1,25 @@
+﻿using AutoMapper;
+using AutoMapper.QueryableExtensions;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using WebMacDonalds.Data;
+using WebMacDonalds.Models.Users;
+
+namespace WebMacDonalds.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController(AppDbAutoriaContext dbContext,
+        IMapper mapper) : ControllerBase
+    {
+        [HttpGet("list")]
+        public async Task<IActionResult> GetUsers()
+        {
+            var items = await dbContext.Users
+                .ProjectTo<UserItemModel>(mapper.ConfigurationProvider)
+                .ToListAsync();
+
+            return Ok(items);
+        }
+    }
+}
---
+++ WebMacDonalds/WebMacDonalds/Mapper/UserMapper.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using WebMacDonalds.Data.Entities.Identity;
+using WebMacDonalds.Models.Users;
+
+namespace WebMacDonalds.Mapper;
+
+public class UserMapper : Profile
+{
+    public UserMapper()
+    {
+        CreateMap<UserEntity, UserItemModel>()
+            .ForMember(dest => dest.FullName, opt => opt.MapFrom(src => $"{src.FirstName} {src.LastName}"))
+            .ForMember(dest => dest.Email, opt => opt.MapFrom(src => src.Email));
+    }
+}
