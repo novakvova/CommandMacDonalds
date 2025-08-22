3d7ffbf6046355153f753e4446cdefb0b10d9697
novakvova <novakvova@gmail.com>
2025-08-22 19:03:33 +0300

    Is good working google auth

--- my-react-ts/src/main.tsx
+++ my-react-ts/src/main.tsx
@@ -2,11 +2,14 @@ import {createRoot} from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 import {BrowserRouter} from "react-router-dom";
+import {GoogleOAuthProvider} from "@react-oauth/google";
 
 createRoot(document.getElementById('root')!).render(
     <>
-        <BrowserRouter>
-            <App/>
-        </BrowserRouter>
+        <GoogleOAuthProvider clientId="627489791815-fm2svvvu2pnqsu6ns1k8qijbgthg7cn0.apps.googleusercontent.com">
+            <BrowserRouter>
+                <App/>
+            </BrowserRouter>
+        </GoogleOAuthProvider>
     </>,
 )
